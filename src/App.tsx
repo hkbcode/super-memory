21251af2508a16355830262e154d467df5741cd6
Hemant Bhoi <56757949+hkbcode@users.noreply.github.com>
2020-12-13 18:59:03 +0530

    :zap: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/Header'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Play with Reactjs Now </h1>
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque autem, culpa tempora ea, illo explicabo alias voluptatibus pariatur ad quaerat ab porro numquam nobis! Nesciunt officia voluptate repellat voluptas eligendi?</p>
+      <Header />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Header = () =>{
+    return(
+        <div>
+            <h2>This is Header comopnents</h2>
+        </div>
+    )
+}
+
+
+
+export default Header
